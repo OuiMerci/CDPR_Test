49327a17ca65728206d40bbf6417a0008b69a134
Ouim <pierremorel.dev@gmail.com>
2018-11-10 14:47:33 +0100

    First Submit

---
+++ Assets/BuddyBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BuddyBehaviour : MonoBehaviour {
+
+    // il faut faire un raycast pour vérifier que buddy voit bien le laser
+    private NavMeshAgent _navAgent;
+
+    //private int _dashHash = Animator.StringToHash("isDashing");
+    //private int _aimingHash = Animator.StringToHash("isAiming");
+
+    // Use this for initialization
+    void Start () {
+        _navAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+    
+    public void SetDest(Vector3 dest)
+    {
+        _navAgent.SetDestination(dest);
+    }
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    const float RAYCAST_MAX_DISTANCE = 100; // Define a const for raycast maxDistance
+
+    [SerializeField] private Transform _laserPointer; //Used to update, the laser that controls the pet's position
+    [SerializeField] private int _laserPointerMask; // We use this to avoid collision with the player's capsule
+    public BuddyBehaviour buddy;
+
+    // Use this for initialization
+    void Start () {
+        _laserPointerMask = ~(1 << 9);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateLaserPosition();
+	}
+
+    private void UpdateLaserPosition()
+    {
+        Camera cam = Camera.main;
+        RaycastHit hit;
+
+        //// si hit collide avec la mummy, on joue l'anim de idle
+
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, RAYCAST_MAX_DISTANCE, _laserPointerMask))
+        {
+            Debug.DrawRay(cam.transform.position, cam.transform.forward, Color.green);
+            _laserPointer.position = cam.transform.position;
+            _laserPointer.eulerAngles = cam.transform.eulerAngles;
+        }
+
+        buddy.SetDest(hit.point);
+    }
+}
